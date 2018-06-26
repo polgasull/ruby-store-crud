12842ca6f751d812963af310b888eede0c3f9cbb
polgasull <pol89.gn@gmail.com>
2018-06-26 16:03:01 +0200

    post and edit products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,30 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :description, :price)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :products
+
+  get '/products', to: 'products#index', as: 'products'
+  get '/products/new', to: 'products#new', as: 'products_new'
+  post '/products', to: 'products#create', as: 'product_create'
+  get '/products/:id/edit', to: 'products#edit', as: 'product_edit'
+  # resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
