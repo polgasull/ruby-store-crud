1098d7920ed3c26460113cec7da8572f6c50e5d4
polgasull <pol89.gn@gmail.com>
2018-07-16 21:31:41 +0200

    Created photo gallery

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get '/admin/:id', to: 'admin#show_project', as: 'admin_show_project'
   get '/admin/:id/edit_project', to: 'admin#edit_project', as: 'admin_edit_project'
   patch '/admin/:id', to: 'admin#update_project', as: 'admin_update_project'
-  delete '/admin/:id', to: 'admin#delete', as: 'admin_delete_project'
+  delete '/admin/:id', to: 'admin#delete_project', as: 'admin_delete_project'
 
   get '/projects', to: 'projects#index', as: "projects"
   get '/projects/:id', to: 'projects#show', as: 'show_project'
